7b7eb379933cdeb333b159d5d42f8b50006a606d
Anshikagupta1306 <anshika13064@gmail.com>
2025-07-30 18:58:18 +0530

    Add files via upload

---
+++ revstrStack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class revstrStack {
+    public static String revstr(String str){
+        Stack<Character> s = new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            sb.append(curr);
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str="anshika";
+        String res=revstr(str);
+        System.out.println(res);
+    }
+}
